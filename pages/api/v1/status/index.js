24606952d275ccd2733cd8575c2339002fb6b33f
temppone <gtvtempone@gmail.com>
2025-01-21 08:03:09 -0300

    feat: `database.js` and `env`

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,11 @@
+import { query } from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await query("SELECT 1 + 1 as sum;");
+
+  console.log(result);
+
+  response.status(200).json({ chave: "são acima da média" });
+}
+
+export default status;
