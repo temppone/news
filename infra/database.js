d2ac4bfba0254ff0ce1de99b14fb7ee15969ab14
Gustavo Tempone <gtvtempone@gmail.com>
2025-03-09 12:05:31 +0000

    feat: 'database.js' and 'client.connect()' more robust

--- infra/database.js
+++ infra/database.js
@@ -9,14 +9,23 @@ export async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  await client.connect();
+  console.log("Postgres credentials", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
 
     return result;
   } catch (error) {
     console.log(error);
+
+    throw error;
   } finally {
     await client.end();
   }
